28090dfe8e1839a7ebe0b9af5366dcd2e6fcf772
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-24 23:59:48 -0400

    done

--- js/script.js
+++ js/script.js
@@ -10,14 +10,22 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit-2-04B/sw.js", {
+    scope: "/ICS2O-Unit-2-04B/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function calculate() {
+  // input
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + "cm²"
 }
